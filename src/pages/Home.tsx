726b3454ca4d6f8df9fb16b37d7f8002c9d17178
Paulo <paulo.henriqq8@gmail.com>
2026-06-02 13:38:46 -0400

    Add Trusted Section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import { Hero } from "../sections/Hero";
+import { Trusted } from "../sections/Trusted";
 
 export function Home(){
     return(
         <main className="h-screen bg-white">
             <Hero/>
+            <Trusted/>
         </main>
     )
 }
---
+++ src/sections/Trusted.tsx
@@ -0,0 +1,34 @@
+import logo1 from '../assets/logos/Logo-1.png'
+import logo2 from '../assets/logos/Logo-2.png'
+import logo3 from '../assets/logos/Logo-3.png'
+import logo4 from '../assets/logos/Logo-4.png'
+import logo5 from '../assets/logos/Logo-5.png'
+import logo6 from '../assets/logos/Logo-6.png'
+
+export function Trusted(){
+    return(
+        <section className="flex flex-col p-4">
+            <p className="text-[15px] font-normal text-[#6F6F6F]">Trusted by:</p>
+            <ul className='grid grid-cols-2 mt-7.5 gap-10'>
+                <li className='p-5'>
+                    <img src={logo1} alt="" />
+                </li>
+                <li className='p-5'>
+                    <img src={logo2} alt="" />
+                </li>
+                <li className='p-5'>
+                    <img src={logo3} alt="" />
+                </li>
+                <li className='p-5'>
+                    <img src={logo4} alt="" />
+                </li>
+                <li className='p-5'>
+                    <img src={logo5} alt="" />
+                </li>
+                <li className='p-5'>
+                    <img src={logo6} alt="" />
+                </li>
+            </ul>
+        </section>
+    )
+}
