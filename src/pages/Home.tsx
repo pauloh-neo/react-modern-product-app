38b4c7a08c4a4e7347751886a08838b131880683
Paulo <paulo.henriqq8@gmail.com>
2026-06-02 13:24:57 -0400

    Add Hero

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { Hero } from "../sections/Hero";
 
 export function Home(){
     return(
-        <main>
+        <main className="h-screen bg-white">
             <Hero/>
         </main>
     )
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,10 +1,16 @@
 import { NavBar } from "../components/NavBar";
-
+import iphone from '../assets/HeroImg.png'
 
 export function Hero(){
     return(
-        <section>
+     
+        <section className="">
             <NavBar/>
+            <div className="flex flex-col p-4 ">
+                <h1 className="mt-10.5 text-6xl font-normal leading-[90%] text-center">Browse everything.</h1>
+                <img className="mt-17.75" src={iphone} alt="" />
+            </div>
         </section>
+       
     )
 }
