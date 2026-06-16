0ac3a8eeca51e8311c7c7f2a400c39643478992a
Paulo <paulo.henriqq8@gmail.com>
2026-06-16 14:43:58 -0400

    Add Hero responsive design

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,15 +1,38 @@
 import { NavBar } from "../components/NavBar";
-import iphone from '../assets/HeroImg.png'
+import mobile from '../assets/HeroImg.png'
+import tablet from  '../assets/ipadTablet.png'
+import desktop from '../assets/IpadDesktop.png'
 
 export function Hero(){
     return(
      
-        <section className="">
+        <section className="md:px-10">
             <NavBar/>
-            <div className="flex flex-col px-4 ">
+                <h1 className="mt-10.5 text-6xl font-normal leading-[90%] text-center md:text-8xl md:font-medium md:mb-20">Browse everything.</h1>
+                <img
+                    src={mobile}
+                    alt="Hero"
+                    className="block md:hidden w-full h-auto object-cover"
+                />
+
+            {/* Tablet (768px - 1023px) */}
+                <img
+                    src={tablet}
+                    alt="Hero"
+                    className="hidden md:block lg:hidden w-full h-auto object-cover"
+                />
+
+            {/* Desktop (≥ 1024px) */}
+                <img
+                    src={desktop}
+                    alt="Hero"
+                    className="hidden lg:block w-full h-auto object-cover"
+                />
+   
+            {/* <div className="flex flex-col px-4 ">
                 <h1 className="mt-10.5 text-6xl font-normal leading-[90%] text-center md:text-8xl md:font-medium">Browse everything.</h1>
-                <img className="mt-17.75" src={iphone} alt="" />
-            </div>
+                <img className="mt-17.75" src={ipadTablet} alt="" />
+            </div> */}
         </section>
        
     )
