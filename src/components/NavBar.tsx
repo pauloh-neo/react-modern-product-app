c44a8f18ed6cfb152019b0a807a3ad4c491294e5
Paulo <paulo.henriqq8@gmail.com>
2026-06-16 13:17:06 -0400

    Add NavBar responsive design for tablet screen

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,55 @@
 import {Menu} from 'lucide-react'
+import { useEffect, useState } from 'react'
+import { Button } from './Button'
+
+type Breakpoints = "mobile" | "tablet" | "desktop"
+
+function getBreakPoints(width: number): Breakpoints{
+    if(width < 768) return 'mobile'
+    if(width < 1024) return 'tablet'
+    return 'desktop'
+}
 
 export function NavBar(){
+
+    const[breakpoint, setBreakPoint] = useState<Breakpoints>(getBreakPoints(window.innerWidth))
+
+    useEffect(() => {
+    function handleResize() {
+      setBreakPoint(getBreakPoints(window.innerWidth));
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
     return(
-        <nav className='flex justify-between p-5 bg-white rounded-b-3xl shadow'>
-            <p className='text-3xl text-black font-medium'>Area</p>
-            <Menu className='text-black size-7'/>
+        <nav className='bg-white md:pt-6.5'>
+            {breakpoint === 'mobile' && 
+            <div className='flex items-center justify-between p-5 rounded-b-3xl shadow'>
+                <p className='text-3xl text-black font-medium'>Area</p>
+                <Menu className='text-black size-7'/>
+            </div>}
+            {breakpoint === 'tablet' && 
+            <div className='flex justify-around'>
+                
+                <p className='text-3xl text-black font-medium'>Area</p>
+                <ul className='flex items-center gap-7 text-black font-medium'>
+                    <li>Benefits</li>
+                    <li>Specifications</li>
+                    <li>How-to</li>
+                    <li>Contact Us</li>
+                </ul>
+
+                <button className='bg-[#485C11] text-white font-medium px-5.5 py-3.5 rounded-4xl w-fit'>
+                    Learn More
+                </button>
+                
+            </div>}
+            
         </nav>
     )
 }
